4c00ddec8500afe01875d07d99a50635f235cfff
Ihor Magur <ihor.magur@devabit.com>
2022-09-15 17:51:50 +0300

    Added router and mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { 
+  createTheme,
+  CssBaseline,
+  ThemeProvider
+} from '@mui/material';
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import { MainLayout } from './layouts/MainLayout/MainLayout';
+import { Routes } from './routes';
 
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: Routes.Home,
+    element: <MainLayout />
+  }
+]);
+
+const theme = createTheme();
+
+export const App: React.FC = () => {
   return (
-    <div />
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
   );
 }
-
-export default App;
