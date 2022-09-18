001829e04a874480b951293a78ca947fa6cafc28
Ihor Magur <ihor.magur@devabit.com>
2022-09-18 18:36:03 +0300

    Added products list

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Box, Typography } from "@mui/material";
 import { useAuth } from '../../contexts/AuthContext';
+import { ProductItems } from '../ProductItems/ProductItems';
 
 export const Dashboard = () => {
   const {currentUser} = useAuth();
   return <Box>
     <Typography variant="h4">Welcome, {currentUser?.displayName ?? "Guest"}</Typography>
+    <ProductItems />
   </Box>
 }
---
+++ src/components/ProductItems/ProductItems.tsx
@@ -0,0 +1,53 @@
+import { Box, Button, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import React, { useState } from 'react';
+
+
+type TProduct = {
+  name: string;
+  qty: number;
+  price: number;
+}
+
+const productsMock: TProduct[] = [
+  {name: "Item 1", qty: 1, price: 12.90},
+  {name: "Item 2", qty: 1, price: 12.90},
+  {name: "Item 3", qty: 22, price: 12.90},
+  {name: "Item 5", qty: 2, price: 12.90},
+  {name: "Item 1", qty: 1, price: 12.90},
+  {name: "Item 1", qty: 5, price: 12.90},
+  {name: "Item 1", qty: 1, price: 12.90},
+  {name: "Item 1", qty: 1, price: 12.90},
+  {name: "Item 1", qty: 1, price: 12.90},
+]
+
+export const ProductItems = () => {
+  const [products, setProducts] = useState<TProduct[]>([]);
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          {["Product name", "Qty", "Price", "Total"].map((label) => <TableCell key={label}>{label}</TableCell>)}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {products.length ? products.map((product, idx) => <TableRow key={idx}>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{product.qty}</TableCell>
+            <TableCell>${product.price.toFixed(2)}</TableCell>
+            <TableCell>${(product.qty*product.price).toFixed(2)}</TableCell>
+          </TableRow>)
+          : <TableRow>
+            <TableCell colSpan={4} align="center">
+              <Box display="flex" alignItems="center" flexDirection="column">
+                <Typography mb={1}>You have no items.</Typography>
+                <Button size="small" variant='outlined'><Add /> Add item</Button>
+              </Box>
+               
+            </TableCell>
+          </TableRow>
+        }
+      </TableBody>
+    </Table>
+  );
+}
