d1c037694eab3c6270879409cfccaa53e33f16b3
Ihor Magur <ihor.magur@devabit.com>
2022-09-18 18:18:34 +0300

    Updated welcome

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Box, Typography } from "@mui/material";
+import { useAuth } from '../../contexts/AuthContext';
 
 export const Dashboard = () => {
+  const {currentUser} = useAuth();
   return <Box>
-    <Typography variant="h3">Home</Typography>
+    <Typography variant="h4">Welcome, {currentUser?.displayName ?? "Guest"}</Typography>
   </Box>
 }
