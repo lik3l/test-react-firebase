543aea1c169b929c85403a15ab2711261e1bd02b
Ihor Magur <ihor.magur@devabit.com>
2022-09-18 19:57:14 +0300

    Saved document

--- src/components/ProductModal/ProductModal.tsx
+++ src/components/ProductModal/ProductModal.tsx
@@ -1,5 +1,8 @@
 import { Box, Button, Dialog, TextField, Typography } from '@mui/material';
+import { doc, setDoc, FirestoreError, collection } from 'firebase/firestore';
 import React, { useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import { db } from '../../firebase';
 import { TModalProps, TProduct } from '../../types';
 
 const blankProduct: TProduct = {
@@ -11,8 +14,34 @@ const blankProduct: TProduct = {
 type TProps = TModalProps & {}
 export const ProductModal: React.FC<TProps> = ({ open, onClose }) => {
   const [itemForm, setItemForm] = useState<TProduct>(blankProduct);
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const {currentUser} = useAuth();
+
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = ({ target: { value, name } }) => {
+    setError('');
     setItemForm({ ...itemForm, [name]: value });
+  }
+  const handleSave = async () => {
+    const { name, price, qty } = itemForm;
+    if (![name, price, qty].every(el => Boolean(el))) {
+      return setError('Please fill all data');
+    }
+    if (!currentUser) {
+      return setError('Please log in');
+    }
+    setError('');
+    setLoading(true);
+    try {
+      const docRef = doc(collection(db, 'users', currentUser.uid, 'products'));
+      await setDoc(docRef, {name, qty, price});
+      onClose();
+    } catch (e) {
+      const err = e as FirestoreError;
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return <Dialog
@@ -20,7 +49,7 @@ export const ProductModal: React.FC<TProps> = ({ open, onClose }) => {
     onClose={onClose}
   >
     <Box p={2}>
-      <Typography mb={2} variant="h4">Add new item:</Typography>
+      <Typography mb={2} align="center" variant="h4">Add new item:</Typography>
       <Box mb={2} display="flex" flexDirection="column" gap={1}>
         <TextField
           onChange={handleChange}
@@ -50,9 +79,10 @@ export const ProductModal: React.FC<TProps> = ({ open, onClose }) => {
           label={"Total"}
           variant="standard" />
       </Box>
+      {error && <Typography mb={1} align='center' color="red">{error}</Typography>}
       <Box display="flex" justifyContent="flex-end" gap={1}>
         <Button onClick={onClose} color="error" variant="outlined">Close</Button>
-        <Button variant="contained">Save</Button>
+        <Button disabled={loading} onClick={handleSave} variant="contained">Save</Button>
       </Box>
     </Box>
   </Dialog>
